a039ff6d1f728c9b4adab9c428026ff7591c4083
admin <admin@admins-MacBook-Pro.local>
2025-05-20 17:27:44 +0800

    fix

--- torchtitan/models/llama3/model.py
+++ torchtitan/models/llama3/model.py
@@ -299,6 +299,11 @@ class TransformerBlock(nn.Module):
         out = h + ffn_output
         return out
 
+    def init_weights(self):
+        for norm in (self.attention_norm, self.ffn_norm):
+            norm.reset_parameters()
+        self.feed_forward.init_weights(self.weight_init_std)
+
 
 class Transformer(nn.Module, ModelProtocol):
     """
