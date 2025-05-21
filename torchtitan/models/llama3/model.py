c9c0e6bc0327f2deca782cea2567d5a79ff017de
admin <admin@admins-MacBook-Pro.local>
2025-05-21 10:36:37 +0800

    fix

--- torchtitan/models/llama3/model.py
+++ torchtitan/models/llama3/model.py
@@ -195,7 +195,8 @@ class Attention(nn.Module):
             rms_norm=True,
             norm_eps=model_args.norm_eps,
             layer_idx=layer_idx,
-            chunk_size=model_args.max_seq_len,
+            # chunk_size=model_args.max_seq_len,
+            chunk_size=512,
         )
 
         # 修正：确保在使用self.hidden_size之前已经初始化
